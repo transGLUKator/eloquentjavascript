c976c638e0617c8c6fb72f0b48c9fabf61e53843
unknown <skhval@SKHVAL.ARTGROUP.local>
2015-10-05 17:10:06 +0300

    Chapter 4: A List

--- index.js
+++ index.js
@@ -19,22 +19,51 @@ function sum(arr) {
 }
 
 //Reversing an array
-function reverseArray(arr){
+function reverseArray(arr) {
     var tempArr = [];
 
-    arr.map(function(item){
+    arr.map(function (item) {
         tempArr.unshift(item);
     });
 
     return tempArr;
 }
 
-function reverseArrayInPlace(arr){
+function reverseArrayInPlace(arr) {
     var initialLength = arr.length;
 
-    arr.map(function(item, index, ar){
+    arr.map(function (item, index, ar) {
         return arr.push(ar[initialLength - index - 1]);
     });
 
     arr.splice(0, initialLength);
+}
+
+//A List
+function arrayToList(arr) {
+    var obj = {};
+    obj.value = arr.shift();
+    obj.rest = arr.length > 0 ? arrayToList(arr) : null;
+    return obj;
+}
+
+function listToArray(obj) {
+    var arr = [];
+    while (obj.rest) {
+        arr.push(obj.value);
+        obj = obj.rest;
+    }
+    arr.push(obj.value);
+    return arr;
+}
+
+function prepend(element, list) {
+    return {
+        value: element,
+        rest: list
+    }
+}
+
+function nth(list, number){
+    return number === 0 ? list.value : nth(list.rest, number - 1);
 }
