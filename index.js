9b1caa7eb6d0f737bb08f1c3f2984a2c8d6b2623
unknown <skhval@SKHVAL.ARTGROUP.local>
2015-10-05 17:27:53 +0300

    Chapter 4: Deep equal

--- index.js
+++ index.js
@@ -64,6 +64,43 @@ function prepend(element, list) {
     }
 }
 
-function nth(list, number){
+function nth(list, number) {
     return number === 0 ? list.value : nth(list.rest, number - 1);
+}
+
+//Deep Comparison
+function deepEqual(object1, object2) {
+
+    for (var propName in object1) {
+        if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+            return false;
+        }
+        else if (typeof object1[propName] != typeof object2[propName]) {
+            return false;
+        }
+    }
+    for (var propName in object2) {
+        if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+            return false;
+        }
+        else if (typeof object1[propName] != typeof object2[propName]) {
+            return false;
+        }
+
+        if (!object1.hasOwnProperty(propName))
+            continue;
+
+        if (object1[propName] instanceof Array && object2[propName] instanceof Array) {
+            if (!object1[propName].equals(object2[propName]))
+                return false;
+        }
+        else if (object1[propName] instanceof Object && object2[propName] instanceof Object) {
+            if (!deepEqual(object1[propName], (object2[propName])))
+                return false;
+        }
+        else if (object1[propName] != object2[propName]) {
+            return false;
+        }
+    }
+    return true;
 }
