be801638786f30778cd26d07b5a0eaf1077c2d65
unknown <skhval@SKHVAL.ARTGROUP.local>
2015-10-05 16:23:01 +0300

    Chapter 4, the sum of a range

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 'use strict';
 
+//Chapter 4: Data Structures: Objects and Arrays
+
+//The sum of a range
+
+function range(start, end) {
+    var arr = [];
+    for (var i = start; i <= end; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+function sum(arr) {
+    return arr.reduce(function (prev, cur) {
+        return prev + cur;
+    });
+}
