8f72ba8563fbbb6d0c5008c0759ea39c19d49265
Sergey Khval <sergeykhval@gmail.com>
2015-10-15 16:22:59 +0300

    Chapter 6: a vector type

--- index.js
+++ index.js
@@ -5,205 +5,228 @@
 //The sum of a range
 
 function range(start, end) {
-    var arr = [];
-    for (var i = start; i <= end; i++) {
-        arr.push(i);
-    }
-    return arr;
+  var arr = [];
+  for (var i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 function sum(arr) {
-    return arr.reduce(function (prev, cur) {
-        return prev + cur;
-    });
+  return arr.reduce(function (prev, cur) {
+    return prev + cur;
+  });
 }
 
 //Reversing an array
 function reverseArray(arr) {
-    var tempArr = [];
+  var tempArr = [];
 
-    arr.map(function (item) {
-        tempArr.unshift(item);
-    });
+  arr.map(function (item) {
+    tempArr.unshift(item);
+  });
 
-    return tempArr;
+  return tempArr;
 }
 
 function reverseArrayInPlace(arr) {
-    var initialLength = arr.length;
+  var initialLength = arr.length;
 
-    arr.map(function (item, index, ar) {
-        return arr.push(ar[initialLength - index - 1]);
-    });
+  arr.map(function (item, index, ar) {
+    return arr.push(ar[initialLength - index - 1]);
+  });
 
-    arr.splice(0, initialLength);
+  arr.splice(0, initialLength);
 }
 
 //A List
 function arrayToList(arr) {
-    var obj = {};
-    obj.value = arr.shift();
-    obj.rest = arr.length > 0 ? arrayToList(arr) : null;
-    return obj;
+  var obj = {};
+  obj.value = arr.shift();
+  obj.rest = arr.length > 0 ? arrayToList(arr) : null;
+  return obj;
 }
 
 function listToArray(obj) {
-    var arr = [];
-    while (obj.rest) {
-        arr.push(obj.value);
-        obj = obj.rest;
-    }
+  var arr = [];
+  while (obj.rest) {
     arr.push(obj.value);
-    return arr;
+    obj = obj.rest;
+  }
+  arr.push(obj.value);
+  return arr;
 }
 
 function prepend(element, list) {
-    return {
-        value: element,
-        rest: list
-    }
+  return {
+    value: element,
+    rest: list
+  }
 }
 
 function nth(list, number) {
-    return number === 0 ? list.value : nth(list.rest, number - 1);
+  return number === 0 ? list.value : nth(list.rest, number - 1);
 }
 
 //Deep Comparison
 function deepEqual(object1, object2) {
 
-    for (var propName in object1) {
-        if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
-            return false;
-        } else if (typeof object1[propName] != typeof object2[propName]) {
-            return false;
-        }
+  for (var propName in object1) {
+    if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+      return false;
+    } else if (typeof object1[propName] != typeof object2[propName]) {
+      return false;
     }
-    for (var propName in object2) {
-        if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
-            return false;
-        } else if (typeof object1[propName] != typeof object2[propName]) {
-            return false;
-        }
-
-        if (!object1.hasOwnProperty(propName))
-            continue;
-
-        if (object1[propName] instanceof Array && object2[propName] instanceof Array) {
-            if (!object1[propName].equals(object2[propName]))
-                return false;
-        } else if (object1[propName] instanceof Object && object2[propName] instanceof Object) {
-            if (!deepEqual(object1[propName], (object2[propName])))
-                return false;
-        } else if (object1[propName] != object2[propName]) {
-            return false;
-        }
+  }
+  for (var propName in object2) {
+    if (object1.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+      return false;
+    } else if (typeof object1[propName] != typeof object2[propName]) {
+      return false;
     }
-    return true;
+
+    if (!object1.hasOwnProperty(propName))
+      continue;
+
+    if (object1[propName] instanceof Array && object2[propName] instanceof Array) {
+      if (!object1[propName].equals(object2[propName]))
+        return false;
+    } else if (object1[propName] instanceof Object && object2[propName] instanceof Object) {
+      if (!deepEqual(object1[propName], (object2[propName])))
+        return false;
+    } else if (object1[propName] != object2[propName]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 //HIGHER ORDER FUNCTIONS
 //Flattening
 function flatten(arr) {
-    return arr.reduce(function (prev, cur) {
-        return prev.concat(cur);
-    });
+  return arr.reduce(function (prev, cur) {
+    return prev.concat(cur);
+  });
 }
 
 //Historical life expectancy
 function average(array) {
-    function plus(a, b) {
-        return a + b;
-    }
-    return array.reduce(plus) / array.length;
+  function plus(a, b) {
+    return a + b;
+  }
+
+  return array.reduce(plus) / array.length;
 }
 
 var ANCESTRY_FILE = "[\n  " + [
-        '{"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"}',
-        '{"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"}',
-        '{"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"}',
-        '{"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"}',
-        '{"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
-        '{"name": "Jan Frans van Brussel", "sex": "m", "born": 1761, "died": 1833, "father": "Jacobus Bernardus van Brussel", "mother":null}',
-        '{"name": "Pauwels van Haverbeke", "sex": "m", "born": 1535, "died": 1582, "father": "N. van Haverbeke", "mother":null}',
-        '{"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"}',
-        '{"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"}',
-        '{"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"}',
-        '{"name": "Pieter Haverbeke", "sex": "m", "born": 1602, "died": 1642, "father": "Lieven van Haverbeke", "mother":null}',
-        '{"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"}',
-        '{"name": "Pieter Bernard Haverbeke", "sex": "m", "born": 1695, "died": 1762, "father": "Willem Haverbeke", "mother": "Petronella Wauters"}',
-        '{"name": "Lieven van Haverbeke", "sex": "m", "born": 1570, "died": 1636, "father": "Pauwels van Haverbeke", "mother": "Lievijne Jans"}',
-        '{"name": "Joanna de Causmaecker", "sex": "f", "born": 1762, "died": 1807, "father": "Bernardus de Causmaecker", "mother":null}',
-        '{"name": "Willem Haverbeke", "sex": "m", "born": 1668, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
-        '{"name": "Pieter Antone Haverbeke", "sex": "m", "born": 1753, "died": 1798, "father": "Jan Francies Haverbeke", "mother": "Petronella de Decker"}',
-        '{"name": "Maria van Brussel", "sex": "f", "born": 1801, "died": 1834, "father": "Jan Frans van Brussel", "mother": "Joanna de Causmaecker"}',
-        '{"name": "Angela Haverbeke", "sex": "f", "born": 1728, "died": 1734, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
-        '{"name": "Elisabeth Haverbeke", "sex": "f", "born": 1711, "died": 1754, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
-        '{"name": "Lievijne Jans", "sex": "f", "born": 1542, "died": 1582, "father":null, "mother":null}',
-        '{"name": "Bernardus de Causmaecker", "sex": "m", "born": 1721, "died": 1789, "father": "Lieven de Causmaecker", "mother": "Livina Haverbeke"}',
-        '{"name": "Jacoba Lammens", "sex": "f", "born": 1699, "died": 1740, "father": "Lieven Lammens", "mother": "Livina de Vrieze"}',
-        '{"name": "Pieter de Decker", "sex": "m", "born": 1705, "died": 1780, "father": "Joos de Decker", "mother": "Petronella van de Steene"}',
-        '{"name": "Joanna de Pape", "sex": "f", "born": 1654, "died": 1723, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
-        '{"name": "Daniel Haverbeke", "sex": "m", "born": 1652, "died": 1723, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
-        '{"name": "Lieven Haverbeke", "sex": "m", "born": 1631, "died": 1676, "father": "Pieter Haverbeke", "mother": "Anna van Hecke"}',
-        '{"name": "Martina de Pape", "sex": "f", "born": 1666, "died": 1727, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
-        '{"name": "Jan Francies Haverbeke", "sex": "m", "born": 1725, "died": 1779, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
-        '{"name": "Maria Haverbeke", "sex": "m", "born": 1905, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
-        '{"name": "Petronella de Decker", "sex": "f", "born": 1731, "died": 1781, "father": "Pieter de Decker", "mother": "Livina Haverbeke"}',
-        '{"name": "Livina Sierens", "sex": "f", "born": 1761, "died": 1826, "father": "Jan Sierens", "mother": "Maria van Waes"}',
-        '{"name": "Laurentia Haverbeke", "sex": "f", "born": 1710, "died": 1786, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
-        '{"name": "Carel Haverbeke", "sex": "m", "born": 1796, "died": 1837, "father": "Pieter Antone Haverbeke", "mother": "Livina Sierens"}',
-        '{"name": "Elisabeth Hercke", "sex": "f", "born": 1632, "died": 1674, "father": "Willem Hercke", "mother": "Margriet de Brabander"}',
-        '{"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
-        '{"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"}',
-        '{"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"}',
-        '{"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}'
-    ].join(",\n  ") + "\n]";
+    '{"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"}',
+    '{"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"}',
+    '{"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"}',
+    '{"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"}',
+    '{"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
+    '{"name": "Jan Frans van Brussel", "sex": "m", "born": 1761, "died": 1833, "father": "Jacobus Bernardus van Brussel", "mother":null}',
+    '{"name": "Pauwels van Haverbeke", "sex": "m", "born": 1535, "died": 1582, "father": "N. van Haverbeke", "mother":null}',
+    '{"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"}',
+    '{"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"}',
+    '{"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"}',
+    '{"name": "Pieter Haverbeke", "sex": "m", "born": 1602, "died": 1642, "father": "Lieven van Haverbeke", "mother":null}',
+    '{"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"}',
+    '{"name": "Pieter Bernard Haverbeke", "sex": "m", "born": 1695, "died": 1762, "father": "Willem Haverbeke", "mother": "Petronella Wauters"}',
+    '{"name": "Lieven van Haverbeke", "sex": "m", "born": 1570, "died": 1636, "father": "Pauwels van Haverbeke", "mother": "Lievijne Jans"}',
+    '{"name": "Joanna de Causmaecker", "sex": "f", "born": 1762, "died": 1807, "father": "Bernardus de Causmaecker", "mother":null}',
+    '{"name": "Willem Haverbeke", "sex": "m", "born": 1668, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+    '{"name": "Pieter Antone Haverbeke", "sex": "m", "born": 1753, "died": 1798, "father": "Jan Francies Haverbeke", "mother": "Petronella de Decker"}',
+    '{"name": "Maria van Brussel", "sex": "f", "born": 1801, "died": 1834, "father": "Jan Frans van Brussel", "mother": "Joanna de Causmaecker"}',
+    '{"name": "Angela Haverbeke", "sex": "f", "born": 1728, "died": 1734, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
+    '{"name": "Elisabeth Haverbeke", "sex": "f", "born": 1711, "died": 1754, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
+    '{"name": "Lievijne Jans", "sex": "f", "born": 1542, "died": 1582, "father":null, "mother":null}',
+    '{"name": "Bernardus de Causmaecker", "sex": "m", "born": 1721, "died": 1789, "father": "Lieven de Causmaecker", "mother": "Livina Haverbeke"}',
+    '{"name": "Jacoba Lammens", "sex": "f", "born": 1699, "died": 1740, "father": "Lieven Lammens", "mother": "Livina de Vrieze"}',
+    '{"name": "Pieter de Decker", "sex": "m", "born": 1705, "died": 1780, "father": "Joos de Decker", "mother": "Petronella van de Steene"}',
+    '{"name": "Joanna de Pape", "sex": "f", "born": 1654, "died": 1723, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
+    '{"name": "Daniel Haverbeke", "sex": "m", "born": 1652, "died": 1723, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+    '{"name": "Lieven Haverbeke", "sex": "m", "born": 1631, "died": 1676, "father": "Pieter Haverbeke", "mother": "Anna van Hecke"}',
+    '{"name": "Martina de Pape", "sex": "f", "born": 1666, "died": 1727, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
+    '{"name": "Jan Francies Haverbeke", "sex": "m", "born": 1725, "died": 1779, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
+    '{"name": "Maria Haverbeke", "sex": "m", "born": 1905, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
+    '{"name": "Petronella de Decker", "sex": "f", "born": 1731, "died": 1781, "father": "Pieter de Decker", "mother": "Livina Haverbeke"}',
+    '{"name": "Livina Sierens", "sex": "f", "born": 1761, "died": 1826, "father": "Jan Sierens", "mother": "Maria van Waes"}',
+    '{"name": "Laurentia Haverbeke", "sex": "f", "born": 1710, "died": 1786, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
+    '{"name": "Carel Haverbeke", "sex": "m", "born": 1796, "died": 1837, "father": "Pieter Antone Haverbeke", "mother": "Livina Sierens"}',
+    '{"name": "Elisabeth Hercke", "sex": "f", "born": 1632, "died": 1674, "father": "Willem Hercke", "mother": "Margriet de Brabander"}',
+    '{"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+    '{"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"}',
+    '{"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"}',
+    '{"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}'
+  ].join(",\n  ") + "\n]";
 
 var ancestors = JSON.parse(ANCESTRY_FILE);
 
 var byName = {};
 
 ancestors.forEach(function (person) {
-    byName[person.name] = person;
+  byName[person.name] = person;
 });
 
 var byCentury = {};
 
 ancestors.forEach(function (person) {
-    var prefix = 'century',
-        century = prefix + Math.ceil(person.died / 100);
+  var prefix = 'century',
+    century = prefix + Math.ceil(person.died / 100);
 
-    if (!byCentury.hasOwnProperty(century)) {
-        byCentury[century] = [];
-        byCentury[century].push(person.died - person.born);
-    } else {
-        byCentury[century].push(person.died - person.born);
-    }
+  if (!byCentury.hasOwnProperty(century)) {
+    byCentury[century] = [];
+    byCentury[century].push(person.died - person.born);
+  } else {
+    byCentury[century].push(person.died - person.born);
+  }
 });
 
 for (var century in byCentury) {
-    if (byCentury.hasOwnProperty(century)) {
-        console.log(century + ': ' + average(byCentury[century]));
-    }
+  if (byCentury.hasOwnProperty(century)) {
+    console.log(century + ': ' + average(byCentury[century]));
+  }
 }
 
 //Every then some
 function every(arr, func) {
-    var i;
+  var i;
 
-    for (i = 0; i < arr.length; i++) {
-        if (!func(arr[i])) return false;
-    }
+  for (i = 0; i < arr.length; i++) {
+    if (!func(arr[i])) return false;
+  }
 
-    return true;
+  return true;
 }
 
 function some(arr, func) {
-    var i;
+  var i;
 
-    for (i = 0; i < arr.length; i++) {
-        if (func(arr[i])) return true;
-    }
+  for (i = 0; i < arr.length; i++) {
+    if (func(arr[i])) return true;
+  }
 
-    return false;
+  return false;
 }
+
+//The secret life of objects
+//A vector type
+
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function (vector) {
+  return new Vector(this.x + vector.x, this.y + vector.y);
+}
+
+Vector.prototype.minus = function (vector) {
+  return new Vector(this.x - vector.x, this.y - vector.y);
+}
+
+Object.defineProperty(Vector.prototype, 'length', {
+  get: function () {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+})
